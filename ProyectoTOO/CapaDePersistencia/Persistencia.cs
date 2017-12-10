098d896536b8c043585f21b49a7fcbfa19795e8a
dani <danielgomezochagavia@hotmail.com>
2017-12-10 12:34:59 +0100

    implementacion de la clase persistencia
    a lo "cutre"

--- ProyectoTOO/CapaDePersistencia/Persistencia.cs
+++ ProyectoTOO/CapaDePersistencia/Persistencia.cs
@@ -12,41 +12,114 @@ namespace CapaDePersistencia
 
         //Todo debe devolver copias. Las clases básicas tienen un método copiar()
 
+        //DUDA IMPORTANTE
+        //Los objetos que se añaden no deberian ser copias tambien?, no vaya a ser que se modifique el 
+        //objeto que esta guardado en un programa externo y se modifique tambien en la coleccion.
+
         //Servicios para dependientes
 
         public static bool anadirDependiente(Dependiente d)
         {
-            throw new NotImplementedException();
+            //recorre la lista de dependientes
+            //si el dependiente no esta en la lista lo añade y devuelve true
+            //si está en la lista, devuelve false.
+            foreach (Dependiente dependiente in BaseDeDatos.Dependientes)
+            {
+                if (dependiente.NSS.Equals(d.NSS))
+                {
+                    return false;
+                }
+            }
+
+            BaseDeDatos.Dependientes.Add(d);
+            return true;
         }
 
         public static bool borrarDependiente(Dependiente d)
         {
-            throw new NotImplementedException();
+            if (!BaseDeDatos.Dependientes.Remove(d))
+            {
+                foreach (Dependiente dependiente in BaseDeDatos.Dependientes)
+                {
+                    if (dependiente.NSS.Equals(d.NSS))
+                    {
+                        BaseDeDatos.Dependientes.Remove(dependiente);
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public static bool contieneDependiente(Dependiente d)
         {
-            throw new NotImplementedException();
+            foreach (Dependiente dependiente in BaseDeDatos.Dependientes)
+            {
+                if (dependiente.NSS.Equals(d.NSS))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static bool darDeBajaDependiente(Dependiente d)
         {
-            throw new NotImplementedException();
+            //no se preocupa de si el dependiente esta de alta o de baja, simplemente devuelve true si lo encuentra y lo da de baja
+            //false en caso contrario
+
+            foreach (Dependiente dependiente in BaseDeDatos.Dependientes)
+            {
+                if (dependiente.NSS.Equals(d.NSS))
+                {
+                    dependiente.EstaActivo = false;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static bool darDeAltaDependiente(Dependiente d)
         {
-            throw new NotImplementedException();
+
+            //no se preocupa de si el dependiente esta de alta o de baja, simplemente devuelve true si lo encuentra y lo da de alta
+            //false en caso contrario
+
+            foreach (Dependiente dependiente in BaseDeDatos.Dependientes)
+            {
+                if (dependiente.NSS.Equals(d.NSS))
+                {
+                    dependiente.EstaActivo = true;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static Dependiente getDependiente(Dependiente d)
         {
-            throw new NotImplementedException();
+            foreach (Dependiente dependiente in BaseDeDatos.Dependientes)
+            {
+                if (dependiente.NSS.Equals(d.NSS))
+                {
+                    return dependiente.copiar();
+                }
+            }
+            return null;
         }
 
         public static List<Dependiente> getDependientesTienda()
         {
-            throw new NotImplementedException();
+            List<Dependiente> aux = new List<Dependiente>();
+            foreach (Dependiente dependiente in BaseDeDatos.Dependientes)
+            {
+                aux.Add(dependiente.copiar());
+            }
+            return aux;
         }
 
         public static List<Venta> getVentasDeDependiente(Dependiente d, DateTime fecha)
@@ -54,7 +127,22 @@ namespace CapaDePersistencia
             //podemos pasarle el día 1 de un mes y año y que solo se fije en estos dos últimos
             //Por ejemplo le paso 1/enero/2017 y devuelve las ventas de enero/2017
 
-            throw new NotImplementedException();
+            List<Venta> aux = new List<Venta>();
+
+            foreach(Venta venta in BaseDeDatos.Ventas)
+            {
+                if(venta.Dependiente.NSS.Equals(d.NSS))
+                {
+                    DateTime auxF = venta.Fecha;
+
+                    if (fecha.Month.Equals(auxF.Month) && fecha.Year.Equals(fecha.Year))
+                    {
+                        aux.Add(venta);
+                    }
+                }
+            }
+
+            return aux;
         }
 
         //Servicios para ventas
@@ -117,37 +205,110 @@ namespace CapaDePersistencia
 
         public static bool anadirArticulo(Articulo a)
         {
-            throw new NotImplementedException();
+            foreach (Articulo articulo in BaseDeDatos.Articulos)
+            {
+                if (articulo.Id.Equals(a.Id))
+                {
+                    return false;
+                }
+            }
+
+            BaseDeDatos.Articulos.Add(a);
+            return true;
         }
 
         public static bool borrarArticulo(Articulo a)
         {
-            throw new NotImplementedException();
+            if (!BaseDeDatos.Articulos.Remove(a))
+            {
+                foreach (Articulo articulo in BaseDeDatos.Articulos)
+                {
+                    if (articulo.Id.Equals(a.Id))
+                    {
+                        BaseDeDatos.Articulos.Remove(articulo);
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public static bool darDeBajaArticulo(Articulo a)
         {
-            throw new NotImplementedException();
+            foreach (Articulo articulo in BaseDeDatos.Articulos)
+            {
+                if (articulo.Id.Equals(a.Id))
+                {
+                    articulo.EstaActivo = false;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static bool darDeAltaArticulo(Articulo a)
         {
-            throw new NotImplementedException();
+            foreach (Articulo articulo in BaseDeDatos.Articulos)
+            {
+                if (articulo.Id.Equals(a.Id))
+                {
+                    articulo.EstaActivo = true;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static Articulo getArticulo(Articulo a)
         {
-            throw new NotImplementedException();
+            foreach (Articulo articulo in BaseDeDatos.Articulos)
+            {
+                if (articulo.Id.Equals(a.Id))
+                {
+                    return articulo.copiar();
+                }
+            }
+            return null;
         }
 
         public static List<Articulo> getTodosArticulo()
         {
-            throw new NotImplementedException();
+            List<Articulo> aux = new List<Articulo>();
+            foreach (Articulo articulo in BaseDeDatos.Articulos)
+            {
+                aux.Add(articulo.copiar());
+            }
+            return aux;
         }
 
         public static List<Venta> getVentasDeArticulo(Articulo a)
         {
-            throw new NotImplementedException();
+            List<Venta> aux = new List<Venta>();
+
+            foreach(Venta venta in BaseDeDatos.Ventas)
+            {
+
+                //MIRAR!!!!!!!!!!!
+                //Se supone que el break sale del segundo for
+                //si no hace eso, entonces podemos estar añadiendo dos veces la misma venta
+                //ademas de estar recorriendo toda las lineas de venta innecesariamente. INEFICIENTE!!!
+
+                foreach(LineaDeVenta linea in venta.LineasDeVenta)
+                {
+                    if(linea.Articulo.Id.Equals(a.Id))
+                    {
+                        aux.Add(venta);
+                        break;
+                    }
+                }
+
+            }
+
+            return aux;
         }
         
     }
